33630a9536190008172e0f7f6fa1e097069bc8f1
VahurP <vahurpaist@ut.ee>
2024-11-08 20:32:03 +0200

    Create mis_käsi.py

---
+++ mis_käsi.py
@@ -0,0 +1,55 @@
+#mõte, et kasutaja sisestab lihtsalt mis käsi tal on preflop ning siis tagastatakse kui suurest hulgast kätest ta käsi parem on.
+mastide_sõnastik = {
+    "risti" : "\u2663",
+    "ärtu" : "\u2665",
+    "ruutu" : "\u2666",
+    "poti" : "\u2660",
+}
+
+kaartide_väärtused = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+kaardid = ['2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♣', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♥', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♠']
+
+def hand_ranking(käsi): #erineva tugevusega käte võrdlemiseks
+    käed = ["kõrge", "paar", "kaks paari", "kolmik", "rida", "mast", "maja", "mastirida", "kuninglik mastirida"]
+    return käed.index(käsi)
+
+def mitmik_maja(laua_järjend): #paar, kaks paari kolmiku ja neliku funktsioon (annad seitse kaarti ning tagastab mis on)
+    luger = 0 #loeme palju paare on (kolmikud j anelikud pole paarid)
+    tagastus = []
+    ilma_mastita = [] #kui sisendis on ka mastid siis oleks hea kuikood selle pärast katki ei läheks, standardne esitus kaartidele on kaart_mast (kokkukirjutatuna) inglisekeeles nt ace of hearts ehk A♥
+    for i in range(7):
+        ilma_mastita.append(laua_järjend[i][0])
+    for kaart in kaartide_väärtused:
+        if ilma_mastita.count(kaart) == 4:
+            return "nelik"
+        elif ilma_mastita.count(kaart) == 3:
+            tagastus += ["kolmik"]
+        elif ilma_mastita.count(kaart) == 2:
+            luger += 1
+    if luger >= 2:
+        if "kolmik" in tagastus:
+            return "maja"
+        else:
+            return "kaks paari"
+    if luger == 1:
+        if "kolmik" in tagastus:
+            return "maja"
+        else:
+            return "paar"
+    if "kolmik" in tagastus:
+        if tagastus.count("kolmik") >= 2:
+            return "maja"
+        return "kolmik"
+    return False
+
+
+def mast(laua_järjend):
+    laua_mastid = [laua_järjend[i][1] for i in range(7)]
+    for mast in laua_mastid:
+        if laua_mastid.count(mast) >= 5:
+            return "mast"
+    return False
+käsi = ["Ax","Ax","2x","9x","8x","6z","7u"]
+print(mitmik_maja(käsi))
+print(mast(käsi))
