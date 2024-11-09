1e21d62cc69b373ffed1272af12919e3569d02dd
VahurP <vahurpaist@ut.ee>
2024-11-09 02:49:12 +0200

    Update mis_käsi.py
    Tegin paar parandust oma koodis ning lisasin ise ka rea operatsiooni, proovi seda rea asja muuta nii, et saaks ka mastirida ning royal flushi tuvastada

--- mis_käsi.py
+++ mis_käsi.py
@@ -1,4 +1,5 @@
-#mõte, et kasutaja sisestab lihtsalt mis käsi tal on preflop ning siis tagastatakse kui suurest hulgast kätest ta käsi parem on.
+#mõte, et kasutaja sisestab lihtsalt mis käsi tal on preflop ning siis tagastatakse kui
+#suurest hulgast kätest ta käsi parem on.
 mastide_sõnastik = {
     "risti" : "\u2663",
     "clubs" : "\u2663",
@@ -19,18 +20,28 @@ mastide_sõnastik = {
 }
 kaartide_väärtused = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+kaartide_väärtused_sõnastik = {'2':1, '3':2, '4':3, '5':4, '6':5, '7':6, '8':7, '9':8, '10':9, 'J':10, 'Q':11, 'K':12, 'A':13}
+
 kaardid = ['2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♣', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♥', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♠']
+
+def kaartide_järjestus(kaart):
+    järjestus = {"A":13, "K":12, "Q":11, "J":10, "10":9, "9":8, "8":7, "7":6, "6":5, "5":4, "4":3, "3":2, "2":1}
+    return järjestus[kaart]
+
 
 def hand_ranking(käsi): #erineva tugevusega käte võrdlemiseks
     käed = ["kõrge", "paar", "kaks paari", "kolmik", "rida", "mast", "maja", "mastirida", "kuninglik mastirida"]
     return käed.index(käsi)
+
+
+#Defineerime funktsioonid millega saame tuvastada mis käsi kasutajal on
 
 def mitmik_maja(laua_järjend): #paar, kaks paari kolmiku ja neliku funktsioon (annad seitse kaarti ning tagastab mis on)
     luger = 0 #loeme palju paare on (kolmikud j anelikud pole paarid)
     tagastus = []
     ilma_mastita = [] #kui sisendis on ka mastid siis oleks hea kuikood selle pärast katki ei läheks, standardne esitus kaartidele on kaart_mast (kokkukirjutatuna) inglisekeeles nt ace of hearts ehk A♥
     for i in range(7):
-        ilma_mastita.append(laua_järjend[i][0])
+        ilma_mastita.append(laua_järjend[i][:-1])
     for kaart in kaartide_väärtused:
         if ilma_mastita.count(kaart) == 4:
             return "nelik"
@@ -56,12 +67,29 @@ def mitmik_maja(laua_järjend): #paar, kaks paari kolmiku ja neliku funktsioon (
 
 
 def mast(laua_järjend):
-    laua_mastid = [laua_järjend[i][1] for i in range(7)]
+    laua_mastid = [laua_järjend[i][-1] for i in range(7)]
     for mast in laua_mastid:
         if laua_mastid.count(mast) >= 5:
             return "mast"
     return False
-käsi = ["Ax","Ax","2x","9x","8x","6z","7u"]
-print(mitmik_maja(käsi))
-print(mast(käsi))
 
+
+def rida(laua_järjend):
+    laua_väärtused = [laua_järjend[i][:-1] for i in range(7)]
+    laua_väärtused = sorted(laua_väärtused, key=kaartide_järjestus)
+    luger = 0
+    if "A" in laua_väärtused and "2" in laua_väärtused and "3" in laua_väärtused and "4" in laua_väärtused and "5" in laua_väärtused:
+        print("AAA")
+        return "rida"
+    for i in range(6):
+        if kaartide_väärtused_sõnastik[laua_väärtused[i+1]]-kaartide_väärtused_sõnastik[laua_väärtused[i]] == 1:
+            luger += 1
+            if luger == 4:
+                return "rida"
+        else:
+            luger = 0
+    return False
+
+
+käsi = ["Kx","Qx","10x","Jx","9x","4z","5u"]
+print(rida(käsi))
